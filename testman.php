70a480faf030a1803eba41685bad473b9353307d
tokushima <rhaco1004@gmail.com>
2014-06-09 15:04:09 +0900

    typo

--- testman.php
+++ testman.php
@@ -1182,7 +1182,7 @@ namespace{
 				if(!empty($c)){
 					$cp = str_replace('\\','/',(($c[0] == '\\') ? substr($c,1) : $c));
 					foreach(explode(PATH_SEPARATOR,get_include_path()) as $p){
-						if(!empty($p) && ($r = realpath($p)) !== false && is_file($p.'/'.$cp.'.php')){
+						if(!empty($p) && ($r = realpath($p)) !== false && is_file($f=$p.'/'.$cp.'.php')){
 							require_once($f);
 								
 							if(class_exists($c,false) || interface_exists($c,false) || trait_exists($c,false)){
