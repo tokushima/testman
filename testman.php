a28cf168f8545aea05a8fd01ec190a69bd8b49ff
tokushima <rhaco1004@gmail.com>
2014-07-03 18:51:03 +0900

    router機能はもたない

--- testman.php
+++ testman.php
@@ -1083,145 +1083,40 @@ namespace testman{
 			self::println($msg,'31');
 		}
 	}
-	/**
-	 * router with built-in web server
-	 */
-	class Router{
-		static private function log($state,$file,$uri=null){
-			$error_log = ini_get('error_log');
-	
-			$data = array(
-					date('Y-m-d H:i:s'),
-					$state,
-					(isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : ''),
-					$file,
-					(isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : ''),
-					(is_file($error_log) ? filesize($error_log) : 0),
-					$uri,
-			);
-			file_put_contents('php://stdout',implode("\t",$data).PHP_EOL);
-		}
-		static private function include_php($filename,$subdir){
-			if(isset($_SERVER['SERVER_NAME']) && isset($_SERVER['SERVER_PORT'])){
-				$_ENV['APP_URL'] = 'http://'.$_SERVER['SERVER_NAME'].(($_SERVER['SERVER_PORT'] != 80) ? (':'.$_SERVER['SERVER_PORT']) : '').$subdir;
-			}
-			include($filename);
-		}
-		static public function start(){
-			if(ini_get('date.timezone') == ''){
-				date_default_timezone_set('Asia/Tokyo');
-			}
-			$dir = getcwd();
-			$subdir = '';
-			$uri = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
-			if(strpos($uri,'?') !== false) list($uri) = explode('?',$uri,2);
-			if(substr($uri,0,1) == '/'){
-				$uri = substr($uri,1);
-			}
-			$uri_exp = explode('/',$uri,2);
-			if(is_dir($uri_exp[0])){
-				$subdir = '/'.$uri_exp[0];
-				$dir = $dir.$subdir;
-					
-				if(isset($uri_exp[1])){
-					$uri_exp = explode('/',$uri_exp[1],2);
-				}
-			}
-			chdir($dir);
-			if(is_file($f=($dir.'/'.implode('/',$uri_exp)))){
-				self::log('success',$f,$uri);
-				$info = pathinfo($f);
-				if(isset($info['extension']) && strtolower($info['extension']) == 'php'){
-					self::include_php($f,$subdir);
-				}else{
-					$mime = function($filename){
-						$ext = (false !== ($p = strrpos($filename,'.'))) ? strtolower(substr($filename,$p+1)) : null;
-						switch($ext){
-							case 'jpg':
-							case 'jpeg': return 'jpeg';
-							case 'png':
-							case 'gif':
-							case 'bmp':
-							case 'tiff': return 'image/'.$ext;
-							case 'css': return 'text/css';
-							case 'txt': return 'text/plain';
-							case 'html': return 'text/html';
-							case 'xml': return 'application/xml';
-							case 'js': return 'text/javascript';
-							case 'flv':
-							case 'swf': return 'application/x-shockwave-flash';
-							case '3gp': return 'video/3gpp';
-							case 'gz':
-							case 'tgz':
-							case 'tar':
-							case 'gz': return 'application/x-compress';
-							case 'csv': return 'text/csv';
-							case null:
-							default:
-								return 'application/octet-stream';
-						}
-					};
-					header('Content-Type: '.$mime($f));
-					readfile($f);
-				}
-			}else if(is_file($f=($dir.'/'.$uri_exp[0])) || is_file($f=($dir.'/'.$uri_exp[0].'.php'))){
-				$_SERVER['PATH_INFO'] = '/'.(isset($uri_exp[1]) ? $uri_exp[1] : '');
-				self::log('success',$f,$uri);
-					
-				self::include_php($f,$subdir,$uri);
-			}else if(is_file($f=($dir.'/index.php'))){
-				$_SERVER['PATH_INFO'] = '/'.implode('/',$uri_exp);
-					
-				self::log('success',$f,$uri);
-				self::include_php($f,$subdir,$uri);
-			}else{
-				header('HTTP/1.1 404 Not Found');
-				self::log('failure','',$uri);
-			}
-		}
-	}
+
 }
 namespace{
-	$linkkey = \testman\Coverage::link();
-	
-	if(isset($_POST[$linkkey]) || isset($_GET[$linkkey])){
-		$linkvars = isset($_POST[$linkkey]) ? $_POST[$linkkey] : (isset($_GET[$linkkey]) ? $_GET[$linkkey] : array());
-		if(isset($_POST[$linkkey])) unset($_POST[$linkkey]);
-		if(isset($_GET[$linkkey])) unset($_GET[$linkkey]);
-
-		if(function_exists('xdebug_get_code_coverage') && isset($linkvars['coverage_data_file'])){
-			register_shutdown_function(function() use($linkvars){
+	if(php_sapi_name() !== 'cli'){
+		$linkkey = \testman\Coverage::link();
+		if(isset($_POST[$linkkey]) || isset($_GET[$linkkey])){
+			$linkvars = isset($_POST[$linkkey]) ? $_POST[$linkkey] : (isset($_GET[$linkkey]) ? $_GET[$linkkey] : array());
+			if(isset($_POST[$linkkey])) unset($_POST[$linkkey]);
+			if(isset($_GET[$linkkey])) unset($_GET[$linkkey]);
+		
+			if(function_exists('xdebug_get_code_coverage') && isset($linkvars['coverage_data_file'])){
 				register_shutdown_function(function() use($linkvars){
-					$coverage_data_file = $linkvars['coverage_data_file'];
-					
-					if(is_file($coverage_data_file.'.target')){
-						$target = explode(PHP_EOL,file_get_contents($coverage_data_file.'.target'));
-						$fp = fopen($coverage_data_file.'.coverage','a');
-	
-						foreach(xdebug_get_code_coverage() as $file_path => $lines){
-							if(false !== ($i = array_search($file_path,$target))){
-								fwrite($fp,json_encode(array($i,$lines)).PHP_EOL);
+					register_shutdown_function(function() use($linkvars){
+						$coverage_data_file = $linkvars['coverage_data_file'];
+							
+						if(is_file($coverage_data_file.'.target')){
+							$target = explode(PHP_EOL,file_get_contents($coverage_data_file.'.target'));
+							$fp = fopen($coverage_data_file.'.coverage','a');
+		
+							foreach(xdebug_get_code_coverage() as $file_path => $lines){
+								if(false !== ($i = array_search($file_path,$target))){
+									fwrite($fp,json_encode(array($i,$lines)).PHP_EOL);
+								}
 							}
+							fclose($fp);
 						}
-						fclose($fp);
-					}
-					xdebug_stop_code_coverage();
+						xdebug_stop_code_coverage();
+					});
 				});
-			});
-			xdebug_start_code_coverage();
+				xdebug_start_code_coverage();
+			}
 		}
 		return;
 	}
-	if(isset($_SERVER['SERVER_SOFTWARE']) && preg_match('/PHP.+Development Server/',$_SERVER['SERVER_SOFTWARE'])){
-		\testman\Router::start();
-		return;
-	}
-	$debug = debug_backtrace(false);
-	if(!(empty($debug) || 
-		(sizeof($debug) == 1 && isset($debug[0]['file']) && basename($debug[0]['file']) == basename(__FILE__,'.php').'.phar')
-	)){
-		return;
-	}	
 	
 	ini_set('display_errors','On');
 	ini_set('html_errors','Off');
